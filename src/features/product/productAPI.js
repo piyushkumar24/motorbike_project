755b132781f6a61ad1e0d1e586622ce30493de8f
Sonu kumar <99523590+SonukumarCSE@users.noreply.github.com>
2023-11-04 17:15:44 +0530

    first commit

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -8,17 +8,51 @@ export function fetchAllProducts() {
     );
   }
   
-  export function fetchProductsByFilters(filter) {
-    // filter = {"category":"smartphone"}
+  export function fetchProductsByFilters(filter,sort,pagination) {
+    // filter = {"category":"smartphone","laptops"}
+    //sort = {_sort:"price",_order="desc"}
+    // pagination = {_page:1,_limit=10} 
     // TODO : on server we will support multi values
     let queryString = '';
-    for(let key in filter){
-      queryString += `${key}=${filter[key]}&`
+    for(let key in filter){ 
+      const categoryValues = filter[key];
+      if(categoryValues.length){
+        const lastCategoryValue = categoryValues[categoryValues.length-1]
+        queryString += `${key}=${lastCategoryValue}&`
+      }
     }
-  
+    for(let key in sort){
+      queryString += `${key}=${sort[key]}&`
+    }
+    console.log(pagination)
+  for(let key in pagination){
+    queryString += `${key}=${pagination[key]}&`
+  }
+
+    
     return new Promise(async (resolve) =>{
       //TODO: we will not hard-code server URL here
       const response = await fetch('http://localhost:8080/products?'+queryString) 
+      const data = await response.json()
+      const totalItems = await response.headers.get('X-Total-Count')
+      resolve({data:{products:data,totalItems:+totalItems}})
+    }
+    );
+  }
+
+  
+  export function fetchCategories() {
+    return new Promise(async (resolve) =>{
+      const response = await fetch('http://localhost:8080/categories') 
+      const data = await response.json()
+      resolve({data})
+    }
+    );
+  }
+  
+  export function fetchBrands() {
+    return new Promise(async (resolve) =>{
+      const response = await fetch('http://localhost:8080/brands') 
       const data = await response.json()
       resolve({data})
     }
